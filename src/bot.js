99c4d7b488d2ed6dc231fddbea9de5de90166fef
Tobias <tobi195h@edu.easj.dk>
2022-06-16 11:59:02 +0200

    Added meme

--- src/bot.js
+++ src/bot.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const axios = require('axios').default;
 const { Client, Intents } = require('discord.js');
+const { MessageEmbed } = require('discord.js')  
 
 const client = new Client({
     intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
@@ -20,6 +21,24 @@ client.on('messageCreate', async (message) => {
             const quotes = await Data()
             let quote = quotes[Math.floor(Math.random() * quotes.length)]
             message.channel.send(`${quote['text']}\n` + `- ${quote['author']}`)
+        }
+
+        if(args[0] === "meme"){
+        //Function used to pick a random element from the array
+        const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
+        //Array used to store the images used in the meme command
+        const images = [
+            'https://i.redd.it/48ac82z924f71.jpg',
+            'https://i.redd.it/ump6iep6ik591.jpg',
+            'https://i.redd.it/r5lonjybim391.jpg'
+        ]
+
+        const embedImage = new MessageEmbed()
+            .setDescription('meme')
+            .setImage(random(images))
+            .setTitle('Star Wars Memes')
+        message.channel.send({ embeds: [embedImage] })
+
         }
     }
 
