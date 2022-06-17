a8f5136d02fa3c9ebc9eda4436f62a6040b7970c
Tobias <tobi195h@edu.easj.dk>
2022-06-17 14:07:19 +0200

    Added automatic roles

--- src/bot.js
+++ src/bot.js
@@ -1,12 +1,16 @@
 require('dotenv').config();
 const axios = require('axios').default;
-const { Client, Intents } = require('discord.js');
+const { Client, Intents, MessageReaction } = require('discord.js');
 const { MessageEmbed } = require('discord.js')  
 
 const client = new Client({
-    intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES]
+    partials: ['CHANNEL', 'GUILD_MEMBER', 'GUILD_SCHEDULED_EVENT', 'MESSAGE', 'REACTION', 'USER'],
+    intents:[Intents.FLAGS.GUILDS, Intents.FLAGS.GUILD_MESSAGES, Intents.FLAGS.GUILD_MESSAGE_REACTIONS],
+    
+    
 });
 const PREFIX = "!"
+
 
 client.on('ready', () => {
     console.log(`${client.user.username} has logged in`)
@@ -16,40 +20,64 @@ client.on('messageCreate', async (message) => {
     console.log(`${message.author.tag} has sent ${message.content}`)
     
     if(message.content.startsWith(PREFIX)){
-        const [CMD_NAME,...args] = message.content.split(PREFIX)
-        if(args[0] === "quote"){
-            const quotes = await Data()
+        //CMD_NAME saves the name connected to the prefix, args becomes an array of all the words split by spaces afterwords
+        const [CMD_NAME,...args] = message.content
+            .trim()
+            .substring(PREFIX.length)
+            .split(/\s+/);
+        if(CMD_NAME === "quote"){
+            const quotes = await Data("https://type.fit/api/quotes")
             let quote = quotes[Math.floor(Math.random() * quotes.length)]
             message.channel.send(`${quote['text']}\n` + `- ${quote['author']}`)
         }
 
-        if(args[0] === "meme"){
-        //Function used to pick a random element from the array
-        const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
-        //Array used to store the images used in the meme command
-        const images = [
-            'https://i.redd.it/48ac82z924f71.jpg',
-            'https://i.redd.it/ump6iep6ik591.jpg',
-            'https://i.redd.it/r5lonjybim391.jpg'
-        ]
+        if(CMD_NAME === "meme"){
+            //Function used to pick a random element from the array
+            const random = (arr) => arr[Math.floor(Math.random() * arr.length)];
+            //Array used to store the images used in the meme command
+            const images = [
+                'https://i.redd.it/48ac82z924f71.jpg',
+                'https://i.redd.it/ump6iep6ik591.jpg',
+                'https://i.redd.it/r5lonjybim391.jpg'
+            ]
 
-        const embedImage = new MessageEmbed()
-            .setDescription('meme')
-            .setImage(random(images))
-            .setTitle('Star Wars Memes')
-        message.channel.send({ embeds: [embedImage] })
+            const embedImage = new MessageEmbed()
+                .setDescription('meme')
+                .setImage(random(images))
+                .setTitle('Star Wars Memes')
+            message.channel.send({ embeds: [embedImage] })
 
         }
-    }
 
-    
+    }
+})
+
+client.on('messageReactionAdd', (reaction, user) => {
+    //Defining the name of the emoji
+    const { name } = reaction.emoji
+    //Remembers the member reacting, used for granting the role
+    const member = reaction.message.guild.members.cache.get(user.id)
+    if(reaction.message.id === '987316603832127518'){
+        //Checking if the name matches one of the cases and adds role 
+        switch(name){
+            case '🟥':
+                member.roles.add('987316022430269450')
+                break;
+            case '🟩':
+                member.roles.add('987315963160588328')
+                break;
+            case '🟦':
+                member.roles.add('987316049793921075')
+                break;
+        }
+    }
 })
 
 client.login(process.env.TOKEN)
 
 
-async function Data(){
-    const response = await axios.get("https://type.fit/api/quotes")
+async function Data(url){
+    const response = await axios.get(url)
     const resData = await response.data
     return resData
 }
